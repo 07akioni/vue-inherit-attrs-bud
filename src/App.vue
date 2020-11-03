eb9aaeb3f5360d14432b89a357a77f9c6e9bb436
07akioni <07akioni2@gmail.com>
2020-11-03 15:56:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <comp-b class="x" />
+</template>
+
+<script>
+import CompB from './components/CompB.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CompB
+  }
+}
+</script>
---
+++ src/components/CompB.vue
@@ -0,0 +1,16 @@
+<template>
+  <comp-a>
+    <div>Comp B</div>
+  </comp-a>
+</template>
+
+<script>
+import CompA from './CompA.vue'
+
+export default {
+  name: 'CompB',
+  components: {
+    CompA
+  }
+}
+</script>
